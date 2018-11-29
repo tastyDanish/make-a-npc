00cf592a007ec587a1e18278c815016d550aeaad
Peter Lansdaal <PLansdaal@jndla.com>
2018-11-29 15:08:19 -0800

    add random rolling with weights and the skeleton for the NPC class

--- app/npc.py
+++ app/npc.py
@@ -3,3 +3,52 @@ Author: Peter Lansdaal
 Date: 2018-11-20
 """
 from app import random_weight
+
+
+_base_stats = {'STR': 10, 'CON': 10, 'DEX': 10, 'INT': 10, 'WIS': 10, 'CHA': 10}
+_stat_array = [15, 14, 13, 12, 10, 8]
+
+_human_stats = {'STR': 10, 'CON': 10, 'DEX': 10, 'INT': 10, 'WIS': 10, 'CHA': 10}
+_elf_stats = {'STR': 5, 'CON': 5, 'DEX': 20, 'INT': 20, 'WIS': 10, 'CHA': 10}
+_race_stats = {'Elf': _elf_stats, 'Human': _human_stats}
+
+
+class NPC:
+    """
+    Creates an NPC following the PC building rules
+    """
+    def __init__(self, name, race):
+        self.name = name
+        self.race = race
+        self.stats = generate_stats(self.race)
+        self.background = generate_background(self.race, self.stats)
+        self.pc_class = generate_class(self.background, self.race, self.stats)
+
+
+def generate_stats(race):
+    """
+    Generates stats for the character, rolling stats by their weights
+    :param race:
+    :type race:
+    :return:
+    :rtype:
+    """
+    my_stats = {}
+    remove_stats = []
+    for stat in _stat_array:
+        chosen_stat = random_weight.roll_with_weights_removal(_race_stats[race], remove_stats)
+        my_stats[chosen_stat] = stat
+        remove_stats.append(chosen_stat)
+    return my_stats
+
+
+def generate_background(race, stats):
+    return None
+
+
+def generate_class(background, race, stats):
+    return None
+
+
+if __name__ == '__main__':
+    print(generate_stats('Elf'))
--- app/random_weight.py
+++ app/random_weight.py
@@ -4,3 +4,40 @@ Date: 2018-11-20
 """
 import random
 
+
+def roll_with_weights(things):
+    """
+    returns a single value from a list of items with weights
+    :param things: a dictionary where the keys are the choices and the values are the weights
+    :type things: dict
+    :return: the chosen item
+    """
+    return random.choices(list(things.keys()), weights=list(things.values()))[0]
+
+
+def roll_with_weights_removal(things, remove):
+    """
+    Rolls with weights, but takes a list of values to remove from the list of choices
+    :param things: the dictionary of things to choose from
+    :type things: dict
+    :param remove: the list of items to remove from the choices dictionary
+    :type remove: list
+    :return: the chosen item
+    """
+    if len(remove) < 1:
+        return roll_with_weights(things)
+    new_dict = {key: things[key] for key in things if key not in remove}
+    if len(new_dict) == 1:
+        return list(new_dict.keys())[0]
+    return roll_with_weights(new_dict)
+
+
+def choose_one(choices):
+    """
+    Takes a list of values and chooses one
+    :param choices: the list of values
+    :type choices: list
+    :return: the choice
+    """
+    choice = int(random.random() * len(choices))
+    return choices[choice]
