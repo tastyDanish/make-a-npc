6c24cb050a8d88404d9210aeaa401254c8084f0c
Peter Lansdaal <pndaal@gmail.com>
2018-12-04 19:55:15 -0800

    html updates and error reporting

---
+++ app/error.py
@@ -0,0 +1,17 @@
+"""
+Author: Peter Lansdaal
+Date: 2018-12-04
+"""
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
